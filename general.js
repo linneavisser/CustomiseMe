9e7e2331aec370c8de6774c88b9d959b1fc77231
evelinafalander <evel0838@stud.kea.dk>
2022-03-08 14:03:32 +0100

    headerOOOPs

--- general.js
+++ general.js
@@ -5,7 +5,9 @@ function menu() {
   let menu = document.querySelector(".menuContent");
   if (menu.style.display === "block") {
     menu.style.display = "none";
+    document.querySelector("#menu").textContent = "Menu";
   } else {
     menu.style.display = "block";
+    document.querySelector("#menu").textContent = "Close";
   }
 }
