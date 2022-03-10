9dc95b65cb14355d5aeb2a6dd5efb1e7aa5b2246
evelinafalander <evel0838@stud.kea.dk>
2022-03-10 17:36:10 +0100

    create soap love

--- productPage.js
+++ productPage.js
@@ -1,6 +1,11 @@
 const urlParams = new URLSearchParams(window.location.search);
 const id = urlParams.get("_id");
-const url = "https://customiseme-0b0d.restdb.io/rest/soaps/" + id;
+const url =
+  "https://customiseme-0b0d.restdb.io/rest/soaps/6226145b0f9df7160011cff2";
+
+//console.log(id);
+//console.log(url);
+//console.log(urlParams.get("_id"));
 
 //console.log(urlParams.get("id"));
 
@@ -23,7 +28,7 @@ function showSoap(soap) {
 
   document.querySelector(".productPageimg img").src = soap.img;
 
-  document.querySelector(".oils").textContent = soap.oils;
-  document.querySelector(".ingredients").textContent = soap.ingredients;
-  document.querySelector(".color").textContent = soap.color;
+  document.querySelector(".oils p").textContent = soap.oils;
+  document.querySelector(".ingredients p").textContent = soap.ingredients;
+  document.querySelector(".color p").textContent = soap.color;
 }
